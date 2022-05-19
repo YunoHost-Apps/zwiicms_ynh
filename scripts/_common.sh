8567b61077f30fdf30d4a8e3b5a8d897a51b43ba
pp-r <64266134+pp-r@users.noreply.github.com>
2022-05-19 19:00:32 +0200

    Update 11.4.02

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 
-pkg_dependencies="php$YNH_DEFAULT_PHP_VERSION-mbstring","php$YNH_DEFAULT_PHP_VERSION-zip","php$YNH_DEFAULT_PHP_VERSION-GD","php$YNH_DEFAULT_PHP_VERSION-exif","php$YNH_DEFAULT_PHP_VERSION-WMLWriter"
+pkg_dependencies="php$YNH_DEFAULT_PHP_VERSION-mbstring","php$YNH_DEFAULT_PHP_VERSION-zip","php$YNH_DEFAULT_PHP_VERSION-GD","php$YNH_DEFAULT_PHP_VERSION-exif","php$YNH_DEFAULT_PHP_VERSION-XMLWriter"
 
 #=================================================
 # PERSONAL HELPERS
